57f381bc64e2a1e721be217f3b1738e806ddf9a5
sohailali_upcred <sohail.ali@upcred.ai>
2023-01-17 17:56:42 +0530

    first commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,24 +1,24 @@
-// module.exports = {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// }
-
 module.exports = {
   plugins: {
-    tailwindcss: {
-       content: [
-         "./index.html",
-         "./src/**/*.{vue,js,ts,jsx,tsx}",
-   ],
-      theme: {
-        extend: {},
-      },
-      plugins: [
-        // require('@tailwindcss/forms'),
-      ],
-    },
+    tailwindcss: {},
     autoprefixer: {},
   },
- }
+}
+
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {
+//        content: [
+//          "./index.html",
+//          "./src/**/*.{vue,js,ts,jsx,tsx}",
+//    ],
+//       theme: {
+//         extend: {},
+//       },
+//       plugins: [
+//         // require('@tailwindcss/forms'),
+//       ],
+//     },
+//     autoprefixer: {},
+//   },
+//  }
